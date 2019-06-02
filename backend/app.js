2907c22dd5b15f7f953c455fef698906da7f797e
percy mugadza <percym@localhost.localdomain>
2019-06-02 12:32:58 +0200

    added posts

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose =require('mongoose');
 
-const Post = require('./models/post')
-
+const Post = require('./models/post');
 
 mongoose.connect('mongodb://localhost/first', {useNewUrlParser: true})
 .then(()=>{
@@ -24,7 +23,7 @@ app.use((req , res , next)=>{
         "Access-Control-Allow-Headers",
         "Origin,X-Requested-With ,Content-Type, Accept");
     res.setHeader(
-            "Access-Control-Allow-Method",
+            "Access-Control-Allow-Methods",
             "GET, POST, PATCH, DELETE,OPTIONS");    
             next();
 });
@@ -35,20 +34,25 @@ app.post('/api/posts',(req, res, next)=>{
        content: req.body.content
    });
    console.log(post);
+   post.save();
    res.status(201).json({
        messsage:'post added'
    })
 });
 app.get('/api/posts',(req, res, next)=>{
-    posts=[
-            {id:'1',title:'First Post', content:'this is the first post content'},
-            {id:'2',title:'Second Post', content:'this is the second post content'},
-            {id:'3',title:'third Post', content:'this is the third post content'},
-        ];
-    res.status(200).json({
-        messsage:'Posts fetched successfully !',
-        posts:posts
+    // posts=[
+    //         {id:'1',title:'First Post', content:'this is the first post content'},
+    //         {id:'2',title:'Second Post', content:'this is the second post content'},
+    //         {id:'3',title:'third Post', content:'this is the third post content'},
+    //     ];
+    Post.find().then(documents => {
+        console.log(documents);
+        res.status(200).json({
+            messsage:'Posts fetched successfully !',
+            posts:documents
+        });
     });
+  
 });
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import {Subject} from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { post } from 'selenium-webdriver/http';
+import { importType } from '@angular/compiler/src/output/output_ast';
+import {map} from 'rxjs/operators';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 @Injectable({providedIn:'root'})
 export class PostsService{
@@ -13,9 +16,20 @@ export class PostsService{
 
     }
     getPosts(){
-        this.http.get<{message:string , posts: Post[]}>('http://localhost:3000/api/posts')
-        .subscribe((postData)=>{
-            this.posts = postData.posts;
+        this.http.get<{message:string, posts: any}>('http://localhost:3000/api/posts')
+        // .pipe(map((postData)=>{
+        //     return postData.posts.map(post=> {
+        //         return {
+        //         title : post.title,
+        //         content : post.content,
+        //         id : post._id 
+        //         }
+        //     });
+        // }))
+        .subscribe((transformedPosts)=>{
+            console.log('____________________posts');
+            console.log(transformedPosts);
+            this.posts = transformedPosts.posts;
             this.postsUpdated.next([...this.posts]);
         });
     }
@@ -28,7 +42,6 @@ export class PostsService{
          const post : Post ={id:null,title:title , content:content};
          this.http.post<{message:string}>('http://localhost:3000/api/posts', post)
          .subscribe((resData)=>{
-             console.log(resData.message);
              this.posts.push(post);
              this.postsUpdated.next([...this.posts]);
          });
