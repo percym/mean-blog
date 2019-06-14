83a80ac8323f05265a1bab9c25e31e400b86356c
percy mugadza <percym@localhost.localdomain>
2019-06-14 08:33:11 +0200

    added timer

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService{
     private isAuthenticated = false;
     private token :string;
     private authStatusListener = new Subject<boolean>();
-
+    private tokenTimer : NodeJS.Timer;
     constructor(private http: HttpClient , private router:Router){
 
     }
@@ -25,15 +25,20 @@ export class AuthService{
 
     login(email:string , password:string){
         const authData:AuthData ={email:email, password:password};
-        this.http.post<{token:string}>('http://localhost:3000/api/user/login', authData)
+        this.http.post<{token:string, expiresIn:number}>('http://localhost:3000/api/user/login', authData)
         .subscribe(response =>{
-            console.log(response);
             this.token = response.token;
             if(this.token ){
+            const expiresInDuration = response.expiresIn;
+            this.tokenTimer = setTimeout(()=>{
+                this.logout();
+
+            },expiresInDuration * 1000)
             this.authStatusListener.next(true);
-            }
             this.isAuthenticated =true;
             this.router.navigate(['/']);
+            }
+           
         });
     }
 
